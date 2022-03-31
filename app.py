061c83f288cdf8eba4a86fc8168c00f0e4dd66c5
Hanafi Haffidz <71706853+gammaraysky@users.noreply.github.com>
2022-03-31 17:36:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,40 @@ import predict_veg_fruit as pvg
 import tensorflow as tf
 from keras.preprocessing import image
 from PIL import Image
-
+import requests
 
 app = Flask(__name__)
+
 
 UPLOAD_FOLDER = './upload/'
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+def download(url: str, dest_folder: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)  # create folder if it does not exist
+
+    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        print("saving to", os.path.abspath(file_path))
+        with open(file_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
+
+
 global model 
-model = tf.keras.models.load_model('http://foxandcat.net/ibm/model_v4.h5')
+download("http://foxandcat.net/ibm/model_v4.h5", dest_folder="model")
+
+model = tf.keras.models.load_model('model/model_v4.h5')
 
 
 @app.route('/', methods=['GET','POST'])
@@ -39,6 +63,8 @@ def submit_file():
             return render_template('index.html', result=result, confidence=f"{percent:.2f}%", writeup=writeup, thumbnail=url_for('static', filename=thumbnail))
         else:
             return render_template('index.html', result=f"I think it's {result} but I'm not so sure, could you try another picture?", confidence=f"{percent:.2f}%", writeup='', thumbnail=thumbnail)
+
+
 
 if __name__ == "__main__":
     app.run()
