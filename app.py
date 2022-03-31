1ee09c46f5f1f9a7b60612b80d3b163ba806b9eb
Hanafi Haffidz <71706853+gammaraysky@users.noreply.github.com>
2022-03-31 23:17:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -17,30 +17,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)  # create folder if it does not exist
-
-    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
-    file_path = os.path.join(dest_folder, filename)
-
-    r = requests.get(url, stream=True)
-    if r.ok:
-        print("saving to", os.path.abspath(file_path))
-        with open(file_path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024 * 8):
-                if chunk:
-                    f.write(chunk)
-                    f.flush()
-                    os.fsync(f.fileno())
-    else:  # HTTP status code 4XX/5XX
-        print("Download failed: status code {}\n{}".format(r.status_code, r.text))
-
-
-global model 
-download("http://foxandcat.net/ibm/model_v4.h5", dest_folder="model")
-
-model = tf.keras.models.load_model('model/model_v4.h5')
+model = tf.keras.models.load_model('model/model_v6.h5')
 
 
 @app.route('/', methods=['GET','POST'])
